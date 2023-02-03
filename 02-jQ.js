b5b227455d333813803c83533de1658ea94cfe57
lestaririanaa@gmail.com <lestaririana77>
2023-02-04 00:26:44 +0700

    jQ-Selectors-Part1

---
+++ 02-jQ.js
@@ -0,0 +1,9 @@
+function func1()
+{
+    //$(element name).action()
+    //document.getElementByTagName(element name)
+
+    // $("div").css('background-color', 'orange');
+    // $("#p1").css('font-style','italic');
+    $(".para").css('font-style','italic')
+}
